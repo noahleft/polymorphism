00051fb54c7ffc45f9d1bb17d568095bf43d543c
ShihFengLin <noahleft@gmail.com>
2020-11-22 13:36:16 +0800

    add pointer casting to show the difference between static and dynamic polymorphism

--- static_polymorphism/main.cpp
+++ static_polymorphism/main.cpp
@@ -6,11 +6,19 @@ int main() {
     {
         Pen pen;
         pen.Plot();
+        // casting the object to other type
+        // the CRTP does not use virtual function
+        // so the behavior is like the simple case
+        // the Plot function will bind to the type
+        Pencil* pencil=(Pencil*)&pen;
+        pencil->Plot();
     }
 
     {
         Pencil pencil;
         pencil.Plot();
+        Pen* pen=(Pen*)&pencil;
+        pen->Plot();
     }
     return 0;
 }
